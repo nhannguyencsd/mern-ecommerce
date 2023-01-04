6f87d2a04832614208958926ce8da65935df932c
nhannguyencsd <nhannguyencsd@gmail.com>
2023-01-04 13:51:13 -0600

    feat(client): create StarRatingFilter component

---
+++ client/src/components/StarRatingFilter/StarRatingFilterIndex.js
@@ -0,0 +1,25 @@
+import classNames from 'classnames/bind';
+import styles from './StarRatingFilter.module.scss';
+import StarRating from '../../components/StarRating/StarRatingIndex';
+
+const cx = classNames.bind(styles);
+
+function StarRatingFilter() {
+    return (
+        <>
+            <h4 className={cx('headline')}>RATING</h4>
+            {[0, 1, 2, 3, 4].map((index) => {
+                return (
+                    <div className={cx('item')} key={index}>
+                        <input type="checkbox" id={index} />
+                        <label htmlFor={index}>
+                            <StarRating fixedValue={5 - index} />
+                        </label>
+                    </div>
+                );
+            })}
+        </>
+    );
+}
+
+export default StarRatingFilter;
--- client/src/pages/ProductListPage/ProductListPageIndex.js
+++ client/src/pages/ProductListPage/ProductListPageIndex.js
@@ -1,6 +1,6 @@
 import classNames from 'classnames/bind';
 import styles from './ProductListPage.module.scss';
-import StarRating from '../../components/StarRating/StarRatingIndex';
+import StarRatingFilter from '../../components/StarRatingFilter/StarRatingFilterIndex';
 
 const cx = classNames.bind(styles);
 
@@ -9,12 +9,8 @@ function ProductListPage() {
         <div className={cx('wrapper')}>
             <div className="grid">
                 <div className="row">
-                    <div className="col lg-3">
-                        <StarRating fixedValue={5} />
-                        <StarRating fixedValue={4} />
-                        <StarRating fixedValue={3} />
-                        <StarRating fixedValue={2} />
-                        <StarRating fixedValue={1} />
+                    <div className="col lg-3 sm-3">
+                        <StarRatingFilter />
                         <div>filter component</div>
                         <div>filter component</div>
                     </div>
