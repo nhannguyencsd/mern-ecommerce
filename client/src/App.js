c5a3eddb0660a3b955a170a121bcc43a64e0e15f
nhannguyencsd <nhannguyencsd@gmail.com>
2023-01-09 13:06:07 -0600

    feat(client): create ScrollToTop component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ProtectedRoutes from './routes/ProtectedRoutes';
 import { publicRoutes, userProtectedRoutes, adminProtectedRoutes } from './routes/createRoutes';
+import ScrollToTop from './components/ScrollToTop/ScrollToTopIndex';
 
 function App() {
     return (
         <BrowserRouter>
+            <ScrollToTop />
             <Routes>
                 {publicRoutes}
                 <Route element={<ProtectedRoutes isAdminRoute={false} />}>{userProtectedRoutes}</Route>
