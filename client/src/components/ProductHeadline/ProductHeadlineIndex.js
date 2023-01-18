d359a2086ef15e98eda586d154324b84438c3d84
nhannguyencsd <nhannguyencsd@gmail.com>
2023-01-18 15:52:22 -0600

    fix(client): rewrite ProductHeadline component

--- client/src/components/ProductHeadline/ProductHeadlineIndex.js
+++ client/src/components/ProductHeadline/ProductHeadlineIndex.js
@@ -1,144 +1,45 @@
+import { useRef } from 'react';
 import classNames from 'classnames/bind';
 import styles from './ProductHeadline.module.scss';
 
 const cx = classNames.bind(styles);
 
-function ProductHeadline() {
+function ProductHeadline({ headlineIcon, headline, content }) {
+    const contentRef = useRef();
+    const expandIconRef = useRef();
+
+    const handleCollapsible = () => {
+        console.log(contentRef.current.style.maxHeight);
+        if (contentRef.current.style.maxHeight) {
+            contentRef.current.style.maxHeight = null;
+            expandIconRef.current.style.transform = 'rotate(0)';
+            contentRef.current.style.marginBottom = 0;
+        } else {
+            contentRef.current.style.maxHeight = contentRef.current.scrollHeight + 'px';
+            expandIconRef.current.style.transform = 'rotate(180deg)';
+            contentRef.current.style.marginBottom = '16px';
+        }
+    };
     return (
         <div className={cx('wrapper')}>
-            {/* feature headline */}
-            <details>
-                <summary>
-                    <div className={cx('title')}>
-                        <svg
-                            fill="#000000"
-                            width="30px"
-                            height="24px"
-                            viewBox="0 0 30 30"
-                            xmlns="http://www.w3.org/2000/svg"
-                            stroke="#000000"
-                            strokeWidth="0.36999999999999996"
-                        >
-                            <g strokeWidth="0"></g>
-                            <g strokeLinecap="round" strokeLinejoin="round" stroke="#CCCCCC" strokeWidth="0.06"></g>
-                            <g>
-                                <path d="M22.005 0c-.194-.002-.372.105-.458.276l-2.197 4.38-4.92.7c-.413.06-.578.56-.278.846l3.805 3.407-.953 4.81c-.07.406.363.715.733.523L22 12.67l4.286 2.273c.37.19.8-.118.732-.522l-.942-4.81 3.77-3.408c.3-.286.136-.787-.278-.846l-4.916-.7-2.2-4.38C22.368.11 22.195.002 22.005 0zM22 1.615l1.863 3.71c.073.148.216.25.38.273l4.168.595-3.227 2.89c-.12.112-.173.276-.145.436l.813 4.08-3.616-1.927c-.147-.076-.322-.076-.47 0l-3.59 1.926.823-4.08c.028-.16-.027-.325-.145-.438l-3.262-2.89 4.166-.594c.165-.023.307-.125.38-.272zM16.5 18c-.822 0-1.5.678-1.5 1.5v9c0 .822.678 1.5 1.5 1.5h9c.822 0 1.5-.678 1.5-1.5v-9c0-.822-.678-1.5-1.5-1.5zm0 1h9c.286 0 .5.214.5.5v9c0 .286-.214.5-.5.5h-9c-.286 0-.5-.214-.5-.5v-9c0-.286.214-.5.5-.5zM1.5 3C.678 3 0 3.678 0 4.5v9c0 .822.678 1.5 1.5 1.5h9c.822 0 1.5-.678 1.5-1.5v-9c0-.822-.678-1.5-1.5-1.5zm0 1h9c.286 0 .5.214.5.5v9c0 .286-.214.5-.5.5h-9c-.286 0-.5-.214-.5-.5v-9c0-.286.214-.5.5-.5zm0 14c-.822 0-1.5.678-1.5 1.5v9c0 .822.678 1.5 1.5 1.5h9c.822 0 1.5-.678 1.5-1.5v-9c0-.822-.678-1.5-1.5-1.5zm0 1h9c.286 0 .5.214.5.5v9c0 .286-.214.5-.5.5h-9c-.286 0-.5-.214-.5-.5v-9c0-.286.214-.5.5-.5z"></path>
-                            </g>
-                        </svg>
-                        <h3>Features</h3>
-                        <svg
-                            className={cx('expand-icon')}
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 16 16"
-                            width="12px"
-                            height="12px"
-                        >
-                            <path d="M1.65 4.24C1.232 3.882.6 3.93.24 4.35c-.359.419-.31 1.05.11 1.41l7 6c.374.32.926.32 1.3 0l7-6c.42-.36.469-.991.11-1.41-.36-.42-.992-.469-1.41-.11L8 9.684 1.65 4.24z" />
-                        </svg>
-                    </div>
-                </summary>
-                <div className={cx('content')}>
-                    <p>
-                        A new feature is nunc lectus eros, volutpat quis suscipit eu, lacinia et justo. Proin turpis mi,
-                        fermentum in feugiat et, sagittis sed nibh. Vivamus convallis felis tempor laoreet eleifend.
-                        Etiam leo dolor.
-                    </p>
-                </div>
-            </details>
-            {/* shipping headline */}
-            <details>
-                <summary>
-                    <div className={cx('title')}>
-                        <svg
-                            fill="#000000"
-                            height="30px"
-                            width="30px"
-                            viewBox="0 0 477.3 477.3"
-                            stroke="#000000"
-                            strokeWidth="0.004773"
-                        >
-                            <g strokeWidth="0"></g>
-                            <g strokeLinecap="round" strokeLinejoin="round"></g>
-                            <g>
-                                {' '}
-                                <g>
-                                    {' '}
-                                    <path d="M451.3,150.55c-15.9-17.2-38.5-26.6-63.6-26.6h-65.5v-36.8c0-6.6-5.4-12-12-12H12c-6.6,0-12,5.4-12,12v263.9 c0,6.6,5.4,12,12,12h47.8c3.9,22.2,23.2,39.1,46.5,39.1s42.6-16.9,46.5-39.1h157.4h26.3c3.9,22.2,23.2,39.1,46.5,39.1 s42.6-16.9,46.5-39.1h35.8c6.6,0,12-5.4,12-12v-124C477.2,195.35,468.3,168.85,451.3,150.55z M24,99.15h274.2v36.8v203.2H150.7 c-6.5-18.2-24-31.3-44.4-31.3s-37.9,13.1-44.4,31.3H24V99.15z M106.3,378.15c-12.8,0-23.2-10.4-23.2-23.2 c0-12.8,10.4-23.2,23.2-23.2s23.2,10.4,23.2,23.2S119.1,378.15,106.3,378.15z M383,378.15c-12.8,0-23.2-10.4-23.2-23.2 c0-12.8,10.4-23.2,23.2-23.2c12.8,0,23.2,10.4,23.2,23.2S395.8,378.15,383,378.15z M453.2,339.05h-25.8 c-6.5-18.2-24-31.3-44.4-31.3s-37.9,13.1-44.4,31.3h-16.3v-191.1h65.5c40.4,0,65.5,30.3,65.5,79.1v112H453.2z"></path>{' '}
-                                </g>{' '}
-                            </g>
-                        </svg>
-                        <h3>Shipping</h3>
-                        <svg
-                            className={cx('expand-icon')}
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 16 16"
-                            width="12px"
-                            height="12px"
-                        >
-                            <path d="M1.65 4.24C1.232 3.882.6 3.93.24 4.35c-.359.419-.31 1.05.11 1.41l7 6c.374.32.926.32 1.3 0l7-6c.42-.36.469-.991.11-1.41-.36-.42-.992-.469-1.41-.11L8 9.684 1.65 4.24z" />
-                        </svg>
-                    </div>
-                </summary>
-                <div className={cx('content')}>
-                    <p>
-                        We ship to sodales sapien massa, euismod imperdiet tellus rutrum nec. Nullam non mauris at
-                        tortor posuere bibendum eget non risus. Vestibulum egestas libero fringilla, pretium eros vitae,
-                        fermentum nisi.
-                    </p>
-                </div>
-            </details>
-            {/* Return Policy headline */}
-            <details>
-                <summary>
-                    <div className={cx('title')}>
-                        <svg
-                            width="30px"
-                            height="24px"
-                            viewBox="0 0 48 48"
-                            fill="none"
-                            xmlns="http://www.w3.org/2000/svg"
-                        >
-                            <g strokeWidth="0"></g>
-                            <g strokeLinecap="round" strokeLinejoin="round"></g>
-                            <g>
-                                {' '}
-                                <path
-                                    d="M12.9998 8L6 14L12.9998 21"
-                                    stroke="#000000"
-                                    strokeWidth="4"
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                ></path>{' '}
-                                <path
-                                    d="M6 14H28.9938C35.8768 14 41.7221 19.6204 41.9904 26.5C42.2739 33.7696 36.2671 40 28.9938 40H11.9984"
-                                    stroke="#000000"
-                                    strokeWidth="4"
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                ></path>{' '}
-                            </g>
-                        </svg>
-                        <h3>Return Policy</h3>
-                        <svg
-                            className={cx('expand-icon')}
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 16 16"
-                            width="12px"
-                            height="12px"
-                        >
-                            <path d="M1.65 4.24C1.232 3.882.6 3.93.24 4.35c-.359.419-.31 1.05.11 1.41l7 6c.374.32.926.32 1.3 0l7-6c.42-.36.469-.991.11-1.41-.36-.42-.992-.469-1.41-.11L8 9.684 1.65 4.24z" />
-                        </svg>
-                    </div>
-                </summary>
-                <div className={cx('content')}>
-                    <p>
-                        Our return policy is aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos
-                        himenaeos. Maecenas fermentum at risus ut tristique. Etiam ut mauris placerat, sollicitudin ante
-                        id, mollis ante. Aliquam molestie nulla a dignissim tempor. Pellentesque mattis vehicula mi, vel
-                        fringilla purus varius
-                    </p>
-                </div>
-            </details>
+            <button className={cx('collapsible')} onClick={handleCollapsible}>
+                {headlineIcon}
+                <h3>{headline}</h3>
+                <svg
+                    className={cx('expand-icon')}
+                    ref={expandIconRef}
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 16 16"
+                    width="12px"
+                    height="12px"
+                >
+                    <path d="M1.65 4.24C1.232 3.882.6 3.93.24 4.35c-.359.419-.31 1.05.11 1.41l7 6c.374.32.926.32 1.3 0l7-6c.42-.36.469-.991.11-1.41-.36-.42-.992-.469-1.41-.11L8 9.684 1.65 4.24z" />
+                </svg>
+            </button>
+
+            <div className={cx('content')} ref={contentRef}>
+                {content}
+            </div>
         </div>
     );
 }
--- client/src/pages/ProductDetailsPage/ProductDetailsPageIndex.js
+++ client/src/pages/ProductDetailsPage/ProductDetailsPageIndex.js
@@ -48,7 +48,96 @@ function ProductDetailsPage() {
                                 dapibus. Duis sagittis elementum commodo. Aliquam egestas pellentesque aliquet.
                             </p>
                             <AddToCart />
-                            <ProductHeadline />
+                            <ProductHeadline
+                                headlineIcon={
+                                    <svg
+                                        fill="#000000"
+                                        width="30px"
+                                        height="24px"
+                                        viewBox="0 0 30 30"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        stroke="#000000"
+                                        strokeWidth="0.36999999999999996"
+                                    >
+                                        <g strokeWidth="0"></g>
+                                        <g
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            stroke="#CCCCCC"
+                                            strokeWidth="0.06"
+                                        ></g>
+                                        <g>
+                                            <path d="M22.005 0c-.194-.002-.372.105-.458.276l-2.197 4.38-4.92.7c-.413.06-.578.56-.278.846l3.805 3.407-.953 4.81c-.07.406.363.715.733.523L22 12.67l4.286 2.273c.37.19.8-.118.732-.522l-.942-4.81 3.77-3.408c.3-.286.136-.787-.278-.846l-4.916-.7-2.2-4.38C22.368.11 22.195.002 22.005 0zM22 1.615l1.863 3.71c.073.148.216.25.38.273l4.168.595-3.227 2.89c-.12.112-.173.276-.145.436l.813 4.08-3.616-1.927c-.147-.076-.322-.076-.47 0l-3.59 1.926.823-4.08c.028-.16-.027-.325-.145-.438l-3.262-2.89 4.166-.594c.165-.023.307-.125.38-.272zM16.5 18c-.822 0-1.5.678-1.5 1.5v9c0 .822.678 1.5 1.5 1.5h9c.822 0 1.5-.678 1.5-1.5v-9c0-.822-.678-1.5-1.5-1.5zm0 1h9c.286 0 .5.214.5.5v9c0 .286-.214.5-.5.5h-9c-.286 0-.5-.214-.5-.5v-9c0-.286.214-.5.5-.5zM1.5 3C.678 3 0 3.678 0 4.5v9c0 .822.678 1.5 1.5 1.5h9c.822 0 1.5-.678 1.5-1.5v-9c0-.822-.678-1.5-1.5-1.5zm0 1h9c.286 0 .5.214.5.5v9c0 .286-.214.5-.5.5h-9c-.286 0-.5-.214-.5-.5v-9c0-.286.214-.5.5-.5zm0 14c-.822 0-1.5.678-1.5 1.5v9c0 .822.678 1.5 1.5 1.5h9c.822 0 1.5-.678 1.5-1.5v-9c0-.822-.678-1.5-1.5-1.5zm0 1h9c.286 0 .5.214.5.5v9c0 .286-.214.5-.5.5h-9c-.286 0-.5-.214-.5-.5v-9c0-.286.214-.5.5-.5z"></path>
+                                        </g>
+                                    </svg>
+                                }
+                                headline="Features"
+                                content="A new feature is nunc lectus eros, volutpat quis suscipit eu, lacinia et justo. Proin turpis mi,
+                                fermentum in feugiat et, sagittis sed nibh. Vivamus convallis felis tempor laoreet eleifend.
+                                Etiam leo dolor."
+                            ></ProductHeadline>
+                            <ProductHeadline
+                                headlineIcon={
+                                    <svg
+                                        fill="#000000"
+                                        height="30px"
+                                        width="30px"
+                                        viewBox="0 0 477.3 477.3"
+                                        stroke="#000000"
+                                        strokeWidth="0.004773"
+                                    >
+                                        <g strokeWidth="0"></g>
+                                        <g strokeLinecap="round" strokeLinejoin="round"></g>
+                                        <g>
+                                            {' '}
+                                            <g>
+                                                {' '}
+                                                <path d="M451.3,150.55c-15.9-17.2-38.5-26.6-63.6-26.6h-65.5v-36.8c0-6.6-5.4-12-12-12H12c-6.6,0-12,5.4-12,12v263.9 c0,6.6,5.4,12,12,12h47.8c3.9,22.2,23.2,39.1,46.5,39.1s42.6-16.9,46.5-39.1h157.4h26.3c3.9,22.2,23.2,39.1,46.5,39.1 s42.6-16.9,46.5-39.1h35.8c6.6,0,12-5.4,12-12v-124C477.2,195.35,468.3,168.85,451.3,150.55z M24,99.15h274.2v36.8v203.2H150.7 c-6.5-18.2-24-31.3-44.4-31.3s-37.9,13.1-44.4,31.3H24V99.15z M106.3,378.15c-12.8,0-23.2-10.4-23.2-23.2 c0-12.8,10.4-23.2,23.2-23.2s23.2,10.4,23.2,23.2S119.1,378.15,106.3,378.15z M383,378.15c-12.8,0-23.2-10.4-23.2-23.2 c0-12.8,10.4-23.2,23.2-23.2c12.8,0,23.2,10.4,23.2,23.2S395.8,378.15,383,378.15z M453.2,339.05h-25.8 c-6.5-18.2-24-31.3-44.4-31.3s-37.9,13.1-44.4,31.3h-16.3v-191.1h65.5c40.4,0,65.5,30.3,65.5,79.1v112H453.2z"></path>{' '}
+                                            </g>{' '}
+                                        </g>
+                                    </svg>
+                                }
+                                headline="Shipping"
+                                content="We ship to sodales sapien massa, euismod imperdiet tellus rutrum nec. Nullam non mauris at
+                                tortor posuere bibendum eget non risus. Vestibulum egestas libero fringilla, pretium eros vitae,
+                                fermentum nisi."
+                            ></ProductHeadline>
+                            <ProductHeadline
+                                headlineIcon={
+                                    <svg
+                                        width="30px"
+                                        height="24px"
+                                        viewBox="0 0 48 48"
+                                        fill="none"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                    >
+                                        <g strokeWidth="0"></g>
+                                        <g strokeLinecap="round" strokeLinejoin="round"></g>
+                                        <g>
+                                            {' '}
+                                            <path
+                                                d="M12.9998 8L6 14L12.9998 21"
+                                                stroke="#000000"
+                                                strokeWidth="4"
+                                                strokeLinecap="round"
+                                                strokeLinejoin="round"
+                                            ></path>{' '}
+                                            <path
+                                                d="M6 14H28.9938C35.8768 14 41.7221 19.6204 41.9904 26.5C42.2739 33.7696 36.2671 40 28.9938 40H11.9984"
+                                                stroke="#000000"
+                                                strokeWidth="4"
+                                                strokeLinecap="round"
+                                                strokeLinejoin="round"
+                                            ></path>{' '}
+                                        </g>
+                                    </svg>
+                                }
+                                headline="Return Policy"
+                                content="Our return policy is aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos
+                                himenaeos. Maecenas fermentum at risus ut tristique. Etiam ut mauris placerat, sollicitudin ante
+                                id, mollis ante. Aliquam molestie nulla a dignissim tempor. Pellentesque mattis vehicula mi, vel
+                                fringilla purus varius"
+                            ></ProductHeadline>
                         </div>
                     </div>
                 </div>
