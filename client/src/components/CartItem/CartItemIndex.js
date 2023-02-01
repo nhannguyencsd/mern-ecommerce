34c7621b1ebcfddbeb7373810f38bad8b7f614cc
nhannguyencsd <nhannguyencsd@gmail.com>
2023-02-01 13:56:43 -0600

    feat(client): add content to CartItem component

--- client/src/components/CartItem/CartItemIndex.js
+++ client/src/components/CartItem/CartItemIndex.js
@@ -1,12 +1,72 @@
+import { Link } from 'react-router-dom';
 import classNames from 'classnames/bind';
 import styles from './CartItem.module.scss';
 
 const cx = classNames.bind(styles);
 
-function CartItem() {
+function CartItem({ item }) {
     return (
         <div className={cx('wrapper')}>
-            <div>Cart Item</div>
+            {/* item information */}
+            <div className={cx('info-cell')}>
+                {/* item image */}
+                <Link className={cx('info-image')} to="/product-details">
+                    <img src={item.image} alt="product img" />
+                </Link>
+                <div className={cx('info-txt')}>
+                    <Link className={cx('info-txt-title')} to="/product-details">
+                        {item.name}
+                    </Link>
+                    {/* options information */}
+                    <ul className={cx('info-txt-options')}>
+                        {/* convert object -> array -> li */}
+                        {Object.entries(item.options).map((option, index) => {
+                            return (
+                                <li key={index}>
+                                    {option[0]}:<strong>{option[1]}</strong>
+                                </li>
+                            );
+                        })}
+                    </ul>
+                </div>
+            </div>
+
+            {/* item value */}
+            <div className={cx('value')}>
+                <div className={cx('price')}>$9999.99</div>
+                <input className={cx('quantity')} type="text" maxLength={2} />
+                <button className={cx('btn-remove')}>
+                    <svg width="15px" height="15px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <g strokeWidth="0"></g>
+                        <g strokeLinecap="round" strokeLinejoin="round"></g>
+                        <g>
+                            {' '}
+                            <path
+                                d="M3 6H21M5 6V20C5 21.1046 5.89543 22 7 22H17C18.1046 22 19 21.1046 19 20V6M8 6V4C8 2.89543 8.89543 2 10 2H14C15.1046 2 16 2.89543 16 4V6"
+                                stroke="#000000"
+                                strokeWidth="2"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                            ></path>{' '}
+                            <path
+                                d="M14 11V17"
+                                stroke="#000000"
+                                strokeWidth="2"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                            ></path>{' '}
+                            <path
+                                d="M10 11V17"
+                                stroke="#000000"
+                                strokeWidth="2"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                            ></path>{' '}
+                        </g>
+                    </svg>
+                    REMOVE
+                </button>
+            </div>
         </div>
     );
 }
--- client/src/pages/CartPage/CartPage.js
+++ client/src/pages/CartPage/CartPage.js
@@ -4,18 +4,52 @@ import CartItem from '../../components/CartItem/CartItemIndex';
 
 const cx = classNames.bind(styles);
 
+const items = [
+    {
+        image: require('../../assets/images/products/samsung-s22.png'),
+        name: 'Samsung Galaxy S22 Ultra 5G',
+        options: {
+            Capacity: '128 GB',
+            Display: 'AMOLED 2X',
+            Color: 'Light Purple',
+        },
+    },
+    {
+        image: require('../../assets/images/products/window-10.png'),
+        name: 'Microsoft Windows 10 Pro',
+        options: {
+            'Operating System': 'Window',
+            Version: 'Pro',
+            Type: '64 Bits',
+        },
+    },
+    {
+        image: require('../../assets/images/products/samsung-s22.png'),
+        name: 'Samsung Galaxy S22 Ultra 5G',
+        options: {
+            Capacity: '256 GB',
+            Display: 'AMOLED 2X',
+            Color: 'Light Purple',
+        },
+    },
+];
+
 function CartPage() {
     return (
         <div className={cx('wrapper')}>
             <div className="grid">
                 <div className="row">
-                    <div className="col lg-8">
+                    {/* cart items */}
+                    <div className="col lg-8 md-12 sm-12">
                         <h2 className={cx('headline')}>
                             Your Cart <span>(3 Items)</span>
                         </h2>
-                        <CartItem />
+                        {items.map((item, index) => {
+                            return <CartItem item={item} key={index} />;
+                        })}
                     </div>
-                    <div className="col lg-4">
+                    {/* cart summary */}
+                    <div className="col lg-4 md-12 sm-12">
                         <h2 className={cx('headline')}>Summary</h2>
                     </div>
                 </div>
